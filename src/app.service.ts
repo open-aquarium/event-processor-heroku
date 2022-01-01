644b4440026c4ad5b60d8ac9f87f9bbaf0eccf53
Guilherme de Lemos <guilherme.eti@gmail.com>
2022-01-01 15:19:49 -0300

    feat(doc): add OpenAPI documentation

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): any {
+    return {
+      appName: "event-processor",
+      appVersion: "0.0.1"
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  // OpenAPI
+  const config = new DocumentBuilder()
+    .setTitle('Event Processor - Open Aquarium')
+    .setDescription('The Event Processor API description')
+    .setVersion('1.0')
+    .addTag('event-processor')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
